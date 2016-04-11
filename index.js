7a7e9801bf325a3e1749a5ab361b94b26da6dde9
PalermoD <palermo.deschamps@gmail.com>
2016-04-11 14:56:34 -0400

    middleware

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ var _ = require('lodash');
 
 //Create the application 
 var app = express();
+
+//Add middleware necessary for REST api's
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
